209a2ce286fb0fc9978e3bbe3cafbaf07f7e9a40
mauroapjr <mauroapjr@outlook.com>
2023-05-10 16:10:55 -0400

    add logo and footer to About

--- sylvia_portfolio/src/components/About.jsx
+++ sylvia_portfolio/src/components/About.jsx
@@ -10,56 +10,63 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 import { faToolbox } from "@fortawesome/free-solid-svg-icons";
 
-import SylviaLogo from "../images/Sylvia_Logo2_salmon.png";
-import BackImg from "../images/background.jpg";
+import SylviaShortLogo from "../images/Sylvia_Logo_Salmon_Short.png";
+import Lines from "../images/Lines.png";
+import ProfilePicture from "../images/Profile_picture.jpg";
 
 function About() {
   return (
     <>
-      <link
-        rel="canonical"
-        href="https://getbootstrap.com/docs/5.3/examples/album/"
-      ></link>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-      ></link>
+      <body>
+        <link
+          rel="canonical"
+          href="https://getbootstrap.com/docs/5.3/examples/album/"
+        ></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        ></link>
+        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
-      <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
-      <header data-bs-theme="dark">
-        <nav
-          className="navbar navbar-expand-lg navbar-dark"
-          style={{ backgroundColor: "#E49393" }}
-        >
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                aria-hidden="true"
-                className="me-2"
-                viewBox="0 0 24 24"
-              ></svg>
-              <img className="sylvia-logo" src={SylviaLogo} alt="Sylvia Logo" />
+        <header data-bs-theme="dark">
+          <nav
+            className="navbar navbar-expand-lg navbar-dark"
+            style={{ backgroundColor: "#E49393" }}
+          >
+            <div className="container-fluid">
+              <a className="navbar-brand" href="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  aria-hidden="true"
+                  className="me-2"
+                  viewBox="0 0 24 24"
+                  backgroundColor="#705846"
+                ></svg>
+                <img
+                  className="sylvia-short-logo"
+                  src={SylviaShortLogo}
+                  alt="sylvia short logo"
+                />
+              </a>
               <ul
                 className="head-item"
                 style={{
                   display: "flex",
+                  marginTop: "80px",
                   justifyContent: "center",
                   textAlign: "center",
                 }}
               >
-                 <div className="head-items" style={{ display: "flex", justifyContent: "center" }}>
-    
+                <div className="head-items" style={{ display: "flex" }}>
                   <a
                     href="/Portfolio"
                     style={{ textDecoration: "none", color: "inherit" }}
@@ -86,94 +93,113 @@ function About() {
                   </a>
                 </div>
               </ul>
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarHeader"
-              aria-controls="navbarHeader"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarHeader">
-              <div className="container">
-                <div className="row">
-                  <div className="col-sm-8 col-md-7 py-4"></div>
-                  <div className="col-sm-4 offset-md-1 py-4">
-                    <ul className="list-unstyled d-flex justify-content-end">
-                      {" "}
-                      <li className="mx-2">
-                        {" "}
-                        <a
-                          href="https://www.linkedin.com/in/sylviabpereira/"
-                          className="text-white"
-                        >
-                          <FontAwesomeIcon
-                            icon={faLinkedin}
-                            size="2xl"
-                            style={{ color: "#bbbcbf" }}
-                          />
-                        </a>
-                      </li>
-                      <li className="mx-2">
-                        {" "}
-                        <a
-                          href="mailto:sylvia.bachiegga@hotmail.com"
-                          className="text-white"
-                        >
-                          <FontAwesomeIcon
-                            icon={faEnvelope}
-                            size="2xl"
-                            style={{ color: "#bbbcbf" }}
-                          />
-                        </a>
-                      </li>
-                      <li className="mx-2">
-                        {" "}
-                        <a href="#" className="text-white">
-                          <FontAwesomeIcon
-                            icon={faFileLines}
-                            size="2xl"
-                            style={{ color: "#bbbcbf" }}
-                          />
-                        </a>
-                      </li>
-                      <li className="mx-2">
-                        {" "}
-                        <a href="/Admin" className="text-white">
-                          <FontAwesomeIcon
-                            icon={faToolbox}
-                            size="2xl"
-                            style={{ color: "#bbbcbf" }}
-                          />
-                        </a>
-                      </li>
-                    </ul>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarHeader"
+                aria-controls="navbarHeader"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarHeader">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-sm-8 col-md-7 py-4"></div>
+                    <div className="col-sm-4 offset-md-1 py-4">
+                      <ul className="list-unstyled"></ul>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </nav>
-      </header>
+          </nav>
+        </header>
 
-      {/* <img
-            className="background-image"
-            src={""}
-            alt="Background"
-            style={{ width: "50%", float: "left", margin: "140px 20px 20px 0" }}
-          /> */}
-      <section
-        className="py-5 text-center"
-        style={{
-          backgroundColor: "#408E91",
-          width: "100%",
-          marginTop: "20px",
-        }}
-      ></section>
+        <img
+          className="line-header"
+          src={Lines}
+          alt="lines"
+          style={{ width: "100%" }}
+        />
+      </body>
+      <main>
+        <img
+          className="profile-picture"
+          src={ProfilePicture}
+          alt="picture"
+          style={{
+            borderRadius: "50%",
+            width: "200px",
+            height: "200px",
+            objectFit: "cover",
+          }}
+        />
+      </main>
+
+      <footer
+        className="text-body-secondary py-5"
+        style={{ backgroundColor: "#408E91" }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-8 col-md-7 py-4"></div>
+            <div className="col-sm-4 offset-md-1 py-4">
+              <ul className="list-unstyled d-flex justify-content-end">
+                {" "}
+                <li className="mx-2">
+                  {" "}
+                  <a
+                    href="https://www.linkedin.com/in/sylviabpereira/"
+                    className="text-white"
+                  >
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="2xl"
+                      style={{ color: "#D8D8D8" }}
+                    />
+                  </a>
+                </li>
+                <li className="mx-2">
+                  {" "}
+                  <a
+                    href="mailto:sylvia.bachiegga@hotmail.com"
+                    className="text-white"
+                  >
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      size="2xl"
+                      style={{ color: "#D8D8D8" }}
+                    />
+                  </a>
+                </li>
+                <li className="mx-2">
+                  {" "}
+                  <a href="#" className="text-white">
+                    <FontAwesomeIcon
+                      icon={faFileLines}
+                      size="2xl"
+                      style={{ color: "#D8D8D8" }}
+                    />
+                  </a>
+                </li>
+                <li className="mx-2">
+                  {" "}
+                  <a href="/Admin" className="text-white">
+                    <FontAwesomeIcon
+                      icon={faToolbox}
+                      size="2xl"
+                      style={{ color: "#D8D8D8" }}
+                    />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
