5c9fb8d998a5ca4a181b39ca0e6c85bfb04467fb
mauroapjr <mauroapjr@outlook.com>
2023-06-13 22:28:28 -0400

    add Office UI Fabric to render .ppt presentations

--- sylvia_portfolio/src/components/Portfolio.jsx
+++ sylvia_portfolio/src/components/Portfolio.jsx
@@ -144,7 +144,51 @@ function Portfolio() {
           </div>
         </div>
 
-        <div className="container-fluid">
+        {/* <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="bg-body-tertiary custom-bg-color me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 py-3">
+                  <h2 className="display-5">TBD</h2>
+                  <p className="lead">Short video that explain...</p>
+                </div>
+                <div
+                  className="bg-body-tertiary shadow-sm mx-auto"
+                  style={{
+                    width: "80%",
+                    height: "300px",
+                    marginBottom: "5%",
+                    borderRadius: "21px 21px 0 0",
+                    backgroundImage: `url(${StorylineImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                ></div>
+                <div className="btn-group">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                    onClick={handleClickStoryline}
+                    style={{
+                      marginBottom: "40%",
+                      backgroundColor: "#245953",
+                      color: "white",
+                    }}
+                  >
+                    Watch Video
+                  </button>
+                </div>
+              </div>
+            </div> */}
+
+
+
+
+
+
+
+
+            <div className="container-fluid">
           <div className="row">
             <div className="col-md-6">
               <div className="bg-body-tertiary custom-bg-color me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
@@ -180,6 +224,14 @@ function Portfolio() {
                 </div>
               </div>
             </div>
+
+
+
+
+
+
+
+
             <div className="col-md-6">
               <div className="bg-body-tertiary custom-bg-color me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
                 <div className="my-3 p-3">
