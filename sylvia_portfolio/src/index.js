ff1a21e825667ea7fcb207f6b00786e5161ee2d1
mauroapjr <mauroapjr@outlook.com>
2023-06-25 10:27:49 -0400

    creating protect routes for portfolio page

--- sylvia_portfolio/src/index.js
+++ sylvia_portfolio/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider, Navigate, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import "./styles/index.css";
 import App from "./App";
@@ -20,50 +20,182 @@ const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest }) => (
   />
 );
 
-const router = createBrowserRouter([
+const routerConfig = [
   {
     path: "/",
     element: <App />,
   },
   {
-    path: "Admin",
+    path: "/Admin",
     element: <Admin />,
   },
   {
-    path: "VideoPage",
+    path: "/VideoPage",
     element: <VideoPage />,
   },
   {
-    path: "StorylinePage",
+    path: "/StorylinePage",
     element: <StorylinePage />,
   },
   {
-    path: "PPTPage",
+    path: "/PPTPage",
     element: <PPTPage />,
   },
   {
-    path: "Blog",
+    path: "/Blog",
     element: <Blog />,
   },
   {
-    path: "Portfolio",
-    element: <ProtectedRoute
-              isAuthenticated={{}} 
-              component={Portfolio}
-             />,
+    path: "/Portfolio",
+    element: <ProtectedRoute isAuthenticated={{}} component={Portfolio} />,
   },
   {
-    path: "About",
+    path: "/About",
     element: <About />,
   },
-]);
+];
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={router}>
-    <React.StrictMode></React.StrictMode>
-  </RouterProvider>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      {routerConfig.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
+
+reportWebVitals();
+export default ProtectedRoute;
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import reportWebVitals from "./reportWebVitals";
+// import { createBrowserRouter, RouterProvider, Navigate, Route, Routes, Switch } from "react-router-dom";
+
+// import "./styles/index.css";
+// import App from "./App";
+// import Admin from "./components/Admin";
+// import VideoPage from "./components/VideoPage";
+// import StorylinePage from "./components/StorylinePage";
+// import PPTPage from "./components/PPTPage";
+// import Blog from "./components/Blog";
+// import Portfolio from "./components/Portfolio";
+// import About from "./components/About";
+
+// const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+//   <Route
+//     {...rest}
+//     element={isAuthenticated ? <Component /> : <Navigate to="/Main" />}
+//   />
+// );
+
+// const routerConfig = [
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/Admin",
+//     element: <Admin />,
+//   },
+//   {
+//     path: "/VideoPage",
+//     element: <VideoPage />,
+//   },
+//   {
+//     path: "/StorylinePage",
+//     element: <StorylinePage />,
+//   },
+//   {
+//     path: "/PPTPage",
+//     element: <PPTPage />,
+//   },
+//   {
+//     path: "/Blog",
+//     element: <Blog />,
+//   },
+//   {
+//     path: "/Portfolio",
+//     element: <ProtectedRoute isAuthenticated={{}} component={Portfolio} />,
+//   },
+//   {
+//     path: "/About",
+//     element: <About />,
+//   },
+// ];
+
+// const router = createBrowserRouter(
+//   <Routes>
+//     {routerConfig.map((route, index) => (
+//       <Route key={index} path={route.path} element={route.element} />
+//     ))}
+//   </Routes>
+// );
+
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const router = createBrowserRouter(
+//   <Routes>
+//     <Route path="/" element={<App />} />
+//     <Route path="Admin" element={<Admin />} />
+//     <Route path="VideoPage" element={<VideoPage />} />
+//     <Route path="StorylinePage" element={<StorylinePage />} />
+//     <Route path="PPTPage" element={<PPTPage />} />
+//     <Route path="Blog" element={<Blog />} />
+//     <Route path="Portfolio" element={<ProtectedRoute isAuthenticated={{}} component={Portfolio} />} />
+//     <Route path="About" element={<About />} />
+//   </Routes>
+// );
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "Admin",
+//     element: <Admin />,
+//   },
+//   {
+//     path: "VideoPage",
+//     element: <VideoPage />,
+//   },
+//   {
+//     path: "StorylinePage",
+//     element: <StorylinePage />,
+//   },
+//   {
+//     path: "PPTPage",
+//     element: <PPTPage />,
+//   },
+//   {
+//     path: "Blog",
+//     element: <Blog />,
+//   },
+//   {
+//     path: "Portfolio",
+//     element: <ProtectedRoute
+//               isAuthenticated={{}} 
+//               component={Portfolio}
+//              />,
+//   },
+//   {
+//     path: "About",
+//     element: <About />,
+//   },
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <RouterProvider router={router}>
+//     <React.StrictMode></React.StrictMode>
+//   </RouterProvider>
+// );
 
 
 
