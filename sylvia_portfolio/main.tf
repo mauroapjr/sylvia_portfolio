c5ea93d3b4d15d08fd6e3db6f99a4c4ebaf21727
mauroapjr <mauroapjr@outlook.com>
2023-06-29 15:43:02 -0400

    install terraform

---
+++ sylvia_portfolio/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "sylvia_server" {
+  ami           = ""
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sylviaServer"
+  }
+}
