d08352ff19caf3bfd4fa9a517f4459b3037bf275
mauroapjr <mauroapjr@outlook.com>
2023-08-20 23:01:03 -0400

    add terraform modules to create bucket/ec2

--- sylvia_portfolio/main.tf
+++ sylvia_portfolio/main.tf
@@ -7,16 +7,44 @@ terraform {
   }
 }
 
+
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
-resource "aws_instance" "sylvia_server" {
-  ami           = ""
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "sylviaServer"
-  }
+# resource "aws_instance" "sylvia_server" {
+#   ami           = ""
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "sylviaServer"
+#   }
+# }
+
+
+module "sylvia_frontend_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  
+  bucket_name = "sylvia-portfolio-bucket"
+  acl         = "public-read"
+}
+
+
+module "sylvia_backend_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  
+  bucket_name = ""
+  acl         = "public-read-write"
+}
+
+
+module "sylvia_backend_ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  
+  instance_count = 1
+  ami            = ""
+  instance_type = "t2.micro"  
+  key_name       = ""
 }
