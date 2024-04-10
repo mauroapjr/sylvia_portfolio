ef9b91a6374ecf126e1f039a8615bbba51c4421e
mauroapjr <mauroapjr@outlook.com>
2024-04-10 15:50:11 -0400

    fixed header

--- sylvia_portfolio/src/components/Blog.jsx
+++ sylvia_portfolio/src/components/Blog.jsx
@@ -4,8 +4,8 @@ import "../styles/blog.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import FlowBackground from "../images/Flow_backgroung.jpg";
-import SylviaLogo from "../images/sylvia-bachiegga-high-resolution-logo-black-on-transparent-background.png";
+import ProfileImage from "../images/Profile_picture.jpg";
+import SylviaLogo from "../images/sylvia-bachiegga-high-resolution-logo-black-on-transparent-background-no-flower.png";
 
 function Blog() {
   const [posts, setPosts] = useState([]);
@@ -27,28 +27,22 @@ function Blog() {
 
   return (
     <>
-      <header data-bs-theme="dark">
+      <header>
         <nav className="navbar">
           <a className="navbar-brand" href="/">
-            <div className="sylvia-logo-container-about">
+            <div className="header-content">
               <img
-                className="sylvia-logo-about"
+                className="profile-image"
+                src={ProfileImage}
+                alt="Profile Image"
+              />
+              <img
+                className="sylvia-logo-main"
                 src={SylviaLogo}
                 alt="Sylvia Logo"
               />
             </div>
           </a>
-
-          <div className="flow-container-about">
-            <img
-              className="flow-background-about"
-              src={FlowBackground}
-              alt="Background about"
-              style={{
-                backgroundImage: `url(${FlowBackground})`,
-              }}
-            />
-          </div>
         </nav>
       </header>
 
